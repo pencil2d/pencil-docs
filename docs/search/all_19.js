43e3333c6a6878cec781e619dde2367805eeded2
Travis CI <deploy@travis-ci.org>
2021-01-27 14:14:17 +0000

    Add generated docs for rev pencil2d/pencil@c1644ed5f2ec62a13beee1ce56a23d7adcea8fe8
    Generated in Travis job #1681.3

--- docs/search/all_19.js
+++ docs/search/all_19.js
@@ -12,7 +12,7 @@ var searchData=
   ['zhuang',['Zhuang',['https://doc.qt.io/qt-5/qlocale.html#Language-enum',1,'QLocale']]],
   ['zimbabwe',['Zimbabwe',['https://doc.qt.io/qt-5/qlocale.html#Country-enum',1,'QLocale']]],
   ['zlibcompression',['ZlibCompression',['https://doc.qt.io/qt-5/qresource.html#Compression-enum',1,'QResource']]],
-  ['zoomin',['ZoomIn',['https://doc.qt.io/qt-5/qkeysequence.html#StandardKey-enum',1,'QKeySequence::ZoomIn()'],['https://doc.qt.io/qt-5/qplaintextedit.html#zoomIn',1,'QPlainTextEdit::zoomIn()'],['https://doc.qt.io/qt-5/qtextedit.html#zoomIn',1,'QTextEdit::zoomIn()']]],
+  ['zoomin',['zoomIn',['https://doc.qt.io/qt-5/qplaintextedit.html#zoomIn',1,'QPlainTextEdit::zoomIn()'],['https://doc.qt.io/qt-5/qtextedit.html#zoomIn',1,'QTextEdit::zoomIn()'],['https://doc.qt.io/qt-5/qkeysequence.html#StandardKey-enum',1,'QKeySequence::ZoomIn()']]],
   ['zoomnativegesture',['ZoomNativeGesture',['https://doc.qt.io/qt-5/qt.html#NativeGestureType-enum',1,'Qt']]],
   ['zoomout',['ZoomOut',['https://doc.qt.io/qt-5/qkeysequence.html#StandardKey-enum',1,'QKeySequence::ZoomOut()'],['https://doc.qt.io/qt-5/qplaintextedit.html#zoomOut',1,'QPlainTextEdit::zoomOut()'],['https://doc.qt.io/qt-5/qtextedit.html#zoomOut',1,'QTextEdit::zoomOut()']]],
   ['zorderchange',['ZOrderChange',['https://doc.qt.io/qt-5/qevent.html#Type-enum',1,'QEvent']]],
